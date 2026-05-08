cdfcfc1c674d608789a0284355bfac69f2cc9103
Hilario Filho <hilariospol@gmail.com>
2026-05-08 20:17:07 -0300

    organizando melhor as funções

--- grafo/grafo_lista_adj.py
+++ grafo/grafo_lista_adj.py
@@ -1,34 +1,67 @@
-from grafo import Grafo
+from grafo.grafo import Grafo
 
 class GrafoListaAdjacencia(Grafo):
     def __init__(self, num_vertices, ponderado):
         #chama o construtor da superclasse Grafo, evita repetições
         super().__init__(num_vertices, ponderado)
 
-        #a lista de adjacencia será inicializada na forma de um dicionário
-        #a chave será o vértice e o valor é uma lista de vizinhos
-        self.adj = {i: [] for i in range(1, num_vertices + 1)}
+        # Dicionário da lista de adjacência
+        # Exemplo:
+        # {
+        #     1: [],
+        #     2: [],
+        #     3: []
+        # }
+        self.adj = {}
+
+        #inicializando todos os vértices com listas vazias
+        for i in range(1, self.num_vertices+1):
+            self.adj[i] = []
 
     def inserir_aresta(self, u, v, peso=None):
+        """
+        Adiciona uma aresta no grafo.
+        Como o grafo é não direcionado,
+        adicionamos nos dois sentidos.
+        
+        Para u: vértice origem
+        Para v: vértice destino
+        Para peso: peso da aresta (opcional)
+        """
+        
+        self.adj[u].append((v, peso))
+        self.adj[v].append((u, peso))
 
-        if self.ponderado:
-            #armazena em tuplas (vizinho, peso)
-            self.adj[u].append((v, peso))
-            self.adj[v].append((u, peso))
-        else:
-            #como não é ponderado, armazena apenas o vizinho
-            self.adj[u].append(v)
-            self.adj[v].append(u)
-
-        self.num_arestas += 1
 
     def Print(self):
-        for i in range(1, num_vertices + 1):
-            if self.ponderado:
-                vizinhos = " ".join([f"{v}({p})" for v, p in self.adj[i]])
-            else:
-                vizinhos = " ".join(map(str, self.adj[i]))
+        """Exibe a lista de adjacência do grafo"""
 
-            print(f"{i}: {vizinhos}")
+        for vertice in self.adj:
+            print(f"{vertice}: ", end="")
 
-            #IMCOMPLETO 
+            for vizinho, peso in self.adj[vertice]:
+
+                if self.ponderado:
+                    print(f"{vizinho}({peso}) ", end="")
+                else:
+                    print(f"{vizinho}: ", end="")
+            print()
+
+    def info(self):
+        "Informa número de vértices, arestas e grau médio"
+        n = self.num_vertices
+
+        #soma dos graus de todos os vértices
+        soma_graus = 0
+
+        for vertice in self.adj:
+            soma_graus += len(self.adj[vertice])
+
+        # Como o grafo não direcionado armazena cada aresta duas vezes:
+        m = soma_graus // 2
+
+        grau_medio = soma_graus / n
+        
+        print(f"n = {n}")
+        print(f"m = {m}")
+        print(f"grau médio = {grau_medio}")
